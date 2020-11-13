181db6593531d2005e56ceb099cc70bb2a0cc26b
Sam Hollyer <rsamhollyer@gmail.com>
2020-11-13 10:45:05 -0600

    added stuff

--- ajax-module.js
+++ ajax-module.js
@@ -9,21 +9,3 @@ export const ajax = (url, callback, method = "GET") => {
 	request.open(method, url);
 	request.send();
 };
-
-// let body = document.querySelector("body");
-// let createULEL = document.createElement("ul");
-// let LIEL1 = document.createElement("li");
-// let LIEL2 = document.createElement("li");
-// let LIEL3 = document.createElement("li");
-// let LIEL4 = document.createElement("li");
-
-// body.append(createULEL);
-// createULEL.innerText = "THis is a list";
-// createULEL.append(LIEL1, LIEL2, LIEL3, LIEL4);
-
-// ajax("./test.txt", (r) => {
-// 	LIEL1.append(r);
-// 	LIEL2.append(r);
-// 	LIEL3.append(r);
-// 	LIEL4.append(r);
-// });
